b86fc3a17410f87ba90dd064381b40c6bceb7e75
Jack Vine <jackv949@gmail.com>
2017-06-21 13:55:46 +0930

    Player movement and camera working

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public float sensitivity = 5.0f;
+
+    public float smoothing = 0.5f;
+
+    void Update()
+    {
+        Vector3 rot = transform.eulerAngles;
+
+        rot.x = Mathf.Lerp(rot.x, rot.x + Input.GetAxis("Mouse Y") * sensitivity * -1, smoothing);
+        rot.y = Mathf.Lerp(rot.y, rot.y + Input.GetAxis("Mouse X") * sensitivity, smoothing);
+
+        transform.eulerAngles = rot;
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float moveSpeed = 10.0f;
+
+    public float gravity = -9.8f;
+    public float jumpForce = 10.0f;
+
+    private Vector3 moveVector;
+
+    private CharacterController controller;
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Vector2 inputVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        if(inputVector.magnitude > 1)
+            inputVector.Normalize();
+
+        if (controller)
+        {
+            Vector3 forward = Camera.main.transform.forward;
+            forward.y = 0;
+            forward.Normalize();
+
+            Vector3 right = Camera.main.transform.right;
+            right.y = 0;
+            right.Normalize();
+
+            moveVector.x = 0;
+            moveVector.z = 0;
+
+            moveVector += right * inputVector.x * moveSpeed;
+            moveVector += forward * inputVector.y * moveSpeed;
+
+            if (Input.GetButtonDown("Jump") && controller.isGrounded)
+                moveVector.y = jumpForce;
+
+            if (!controller.isGrounded)
+                moveVector.y += gravity * Time.deltaTime;
+            else if (moveVector.y <= 0)
+                moveVector.y = 0;
+
+            controller.Move(moveVector * Time.deltaTime);
+        }
+    }
+}
